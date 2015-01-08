35d3724fca4cef5aad879519c3cea423495632c8
Bittech <you@example.com>
2015-01-07 21:38:32 -0500

    add file

---
+++ JeSpecialProject/src/com/bit/test/B.java
@@ -0,0 +1,40 @@
+package com.bit.test;
+
+public class B 
+{
+	int a=8;
+	double b=3.5;
+	String name="fgf";
+	
+	void m1()
+	{
+		System.out.println("fjhg");
+		
+	}
+	int m2()
+	{
+		int c=8;
+		System.out.println(a);
+		System.out.println("iuyrt");
+		return c;
+	}
+	void m3(int e,int f)
+	{
+		
+		System.out.println("gu");
+	}
+	void m4(int g,int h,int k)
+	{
+		System.out.println(g+h-k);
+		
+	}
+	B(int x)
+	{
+		a=x;
+	}
+	B(int x,int w)
+	{
+		System.out.println("gh");
+	}
+
+}
